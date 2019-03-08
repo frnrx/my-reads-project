1f02ed5cd60d020a23158a90330699af87493016
Gabriela Froener <gabriela.froener@gmail.com>
2019-03-08 13:35:23 -0300

    Added a Select feature to Search page's items

--- src/Search.js
+++ src/Search.js
@@ -77,14 +77,22 @@ class Search extends Component {
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                    <div className='book-title'>
-                      {book.title}
+                    <div className="book-shelf-changer">
+                      <select>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                      </select>
                     </div>
-                    <div className='book-authors'>
-                      {book.authors.map((author, i) => (
-                        <p key={i}>{author}</p>
-                      ))}
-                    </div>
+                  </div>
+                  <div className='book-title'>
+                    {book.title}
+                  </div>
+                  <div className='book-authors'>
+                    {book.authors.map((author, i) => (
+                      <p key={i}>{author}</p>
+                    ))}
                   </div>
                 </div>
               </li>
